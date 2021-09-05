d49343be8e6e48356fbd29606946ac9fe2daffac
a3510377 <a102009102009@gmail.com>
2021-09-05 19:48:19 +0800

    debg

--- core/oauth2.js
+++ core/oauth2.js
@@ -4,6 +4,14 @@ const fetch = require("node-fetch");
 module.exports = {
     getDiscordToken: (code, res) => {
         /* 抓取 discord Token */
+        console.log(new URLSearchParams({
+            client_secret: tokes.discord.client_secret,
+            redirect_uri: tokes.discord.redirect_uri,
+            client_id: tokes.discord.client_id,
+            grant_type: "authorization_code",
+            scope: "identify",
+            code: code,
+        }));
         fetch("https://discord.com/api/oauth2/token", {
             method: "POST",
             headers: {
