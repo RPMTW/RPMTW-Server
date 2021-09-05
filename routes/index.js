af62025292b3396d4e19f6d4e0c1ebc900badbf5
a3510377 <a102009102009@gmail.com>
2021-09-05 09:29:33 +0800

    test get ip

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 router.get("/", function (req, res) {
+  console.log(req.headers['x-forwarded-for'] || req.connection.remoteAddress);
   res.render("index.html", );
 });
 
