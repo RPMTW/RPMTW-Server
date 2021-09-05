2c307c16c5dc5cd660f30f881b095c5dbbe02703
菘菘 <rrt467778@gmail.com>
2021-09-05 14:40:52 +0800

    post create user

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,19 @@
 const { CreateUser } = require('../function/auth/Users');
 var router = require('express').Router();
+const bodyParser = require('body-parser');
 
 function authRouter(sequelize) {
-    router.get('/user/create', async (req, res) => {
-        res.json(await CreateUser(sequelize, "teaaast", "teaaaast", "test")).status(200);
+    router.post('/user/create', bodyParser.json(), async (req, res) => {
+        let data = req.body;
+        try {
+            return res.json(await CreateUser(sequelize, data.UserName, data.Email, data.Password)).status(200);
+        } catch (error) {
+            return res.json({
+                message: "Parameter Error"
+            }).status(400);
+        }
     });
     return router;
 }
-
 
 module.exports = authRouter;
