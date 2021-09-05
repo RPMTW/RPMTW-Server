e3e72f67a3316678318a6ea1ca639672a147fbd7
a3510377 <a102009102009@gmail.com>
2021-09-05 17:44:10 +0800

    移除ip ::ffff:

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ function init(expansion) {
   return Object.assign(router, expansion)
     .get("/ip", function (req, res) {
       res.json({
-        ip: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+        ip: (req.headers['x-forwarded-for'] || req.connection.remoteAddress).replace("::ffff:", ""),
         code: 200
       }).status(200)
     })
