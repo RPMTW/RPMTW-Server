08f358bc83a8de5108b2d395c3a3a19dacdaeac3
a3510377 <a102009102009@gmail.com>
2021-09-05 20:26:46 +0800

    debug

--- core/oauth2.js
+++ core/oauth2.js
@@ -1,16 +1,24 @@
 const tokes = require('../env');
 const fetch = require("node-fetch");
-
-module.exports = {
-    getDiscordToken: (code, res) => {
-        /* 抓取 discord Token */
-        fetch("https://discord.com/api/oauth2/token?" + `client_id=${tokes.discord.client_id}&redirect_uri=${tokes.discord.redirect_uri}&client_secret=${tokes.discord.client_secret}&grant_type=authorization_code&scope=identify&code=${code}`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded"
-            }
-        }).then(d => d.json()).then(json => {
-            res.json(json)
+let getDiscordToken = (code, res) => {
+    /* 抓取 discord Token */
+    fetch(`${sets.discord.API}/oauth2/token`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: new URLSearchParams({
+            client_id: tokes.discord.client_id,
+            client_secret: tokes.discord.client_secret,
+            grant_type: "authorization_code",
+            scope: "identify",
+            redirect_uri: tokes.discord.redirect_uri,
+            code: code,
         })
-    }
+    }).then(d => d.json()).then(json => {
+        res.json(json)
+    })
+}
+module.exports = {
+    getDiscordToken
 }
