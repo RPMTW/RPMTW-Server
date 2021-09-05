18de4e2057f2daa6a7652278db9915ba1c79f550
a3510377 <a102009102009@gmail.com>
2021-09-05 19:27:36 +0800

    add .env

---
+++ env.js
@@ -0,0 +1,10 @@
+module.exports = {
+    discord: {
+        client_id: 880821114014666753,
+        client_secret: process.env.discord_Secret,
+        redirect_uri: "https://rpmtw-wki-server.herokuapp.com/oauth2/discord/callback"
+    },
+    google: {
+
+    }
+}
