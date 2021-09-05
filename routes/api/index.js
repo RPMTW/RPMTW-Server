5f38df46286c0ed1b8c55d08612a16897c93cd32
a3510377 <a102009102009@gmail.com>
2021-09-05 13:52:54 +0800

    添加請求上限 429

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,17 @@
 /* 根路徑 /api */
 const router = require('express').Router();
+const rateLimit = require('express-rate-limit')
 
-router.use("/v1", require('./v1'))
+let limiter = rateLimit({
+    windowMs: 1 * 60 * 1e3,
+    max: 100,
+    message: "請求過多，請稍後在試",
+    statusCode: 429,
+})
+
+router
+    .use(limiter)
+    .use("/v1", require('./v1'))
 
 router.get("/", function (req, res) {
     res.json({
