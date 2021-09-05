c115f04b4b70d551dba737e1fe9663163415aa37
a3510377 <a102009102009@gmail.com>
2021-09-05 20:12:52 +0800

    debug

--- core/oauth2.js
+++ core/oauth2.js
@@ -4,7 +4,7 @@ const fetch = require("node-fetch");
 module.exports = {
     getDiscordToken: (code, res) => {
         /* 抓取 discord Token */
-        fetch("https://discord.com/api/oauth2/token?" + `client_secret=${tokes.discord.client_secret}&redirect_uri=${tokes.discord.redirect_uri}&client_id=${tokes.discord.client_id}&grant_type=authorization_code&scope=identify&code=${code}`, {
+        fetch("https://discord.com/api/oauth2/token?" + `client_id=${tokes.discord.client_id}&redirect_uri=${tokes.discord.redirect_uri}&client_secret=${tokes.discord.client_secret}&grant_type=authorization_code&scope=identify&code=${code}`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/x-www-form-urlencoded"
