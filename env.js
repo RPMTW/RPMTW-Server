ed2dbed9fc9916d373ec9afc4725a53118cf4e14
a3510377 <a102009102009@gmail.com>
2021-09-06 18:07:22 +0800

    Update env.js

--- env.js
+++ env.js
@@ -2,7 +2,7 @@ module.exports = {
     discord: {
         client_id: 880821114014666753,
         client_secret: process.env.discord_Secret,
-        redirect_uri: "https://rpmtw-wki-server.herokuapp.com/oauth2/discord/callback"
+        redirect_uri: "https://rpmtw-wki-server.herokuapp.com/" // oauth2/discord/callback
     },
     google: {
 
